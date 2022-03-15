244d124b56a72d74086a578af810c118a57a3694
Murilo Bohlke <murilo.bohlke@rubcube.com>
2022-03-14 22:52:13 -0300

    splash screen in html

--- site/lib/site.dart
+++ site/lib/site.dart
@@ -2,8 +2,26 @@ import 'package:flutter/material.dart';
 import 'package:flutter/gestures.dart';
 import 'package:site/views/main_page.dart';
 
-class Site extends StatelessWidget {
+class Site extends StatefulWidget {
   const Site({Key? key}) : super(key: key);
+
+  @override
+  State<Site> createState() => _SiteState();
+}
+
+class _SiteState extends State<Site> with SingleTickerProviderStateMixin {
+  late AnimationController animationController;
+  double _opacity = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance?.addPostFrameCallback((_) {
+      setState(() {
+        _opacity = 1;
+      });
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -11,7 +29,11 @@ class Site extends StatelessWidget {
       scrollBehavior: MyCustomScrollBehavior(),
       debugShowCheckedModeBanner: false,
       title: 'Meu site',
-      home: const MainPage(),
+      home: AnimatedOpacity(
+        duration: const Duration(milliseconds: 600),
+        opacity: _opacity,
+        child: const MainPage(),
+      ),
     );
   }
 }
